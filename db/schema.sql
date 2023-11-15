75fc64d8bda897afd4d0e3c644ffb157fad70d16
jameel914 <jameelibrahim107@gmail.com>
2023-11-15 18:29:50 -0500

    schema is done

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+DROP DATABASE IF EXISTS foods_dev;
+CREATE DATABSE foods_dev;
+
+\c foods_dev
+
+CREATE TABLE foods (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    image TEXT,
+    calories INTEGER,
+    price DECIMAL NOT NULL,
+    is_veg BOOLEAN,
+    is_spicy BOOLEAN NOT NULL,
+    description TEXT,
+);
