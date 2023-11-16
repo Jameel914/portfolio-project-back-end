c4e1e2f8a40556947ff8cb021e9c5a2f3e859bcf
jameel914 <jameelibrahim107@gmail.com>
2023-11-15 20:30:53 -0500

    seed is done

---
+++ db/seed.sql
@@ -0,0 +1,22 @@
+\c foods_dev;
+
+INSERT INTO foods (name, image, calories, price, is_veg, is_spicy, description) VALUES
+('Naan Bread', 'https://images.pexels.com/photos/1117862/pexels-photo-1117862.jpeg?auto=compress&cs=tinysrgb&w=600', 262, 3.50, true, false, 'It made from wheat dough, which is traditionally baked against the side of a tandoor oven.'),
+('Chicken Curry', 'https://images.pexels.com/photos/2474661/pexels-photo-2474661.jpeg?auto=compress&cs=tinysrgb&w=600', 296, 15.99, false, true, 'It consists of chicken stewed in an onion and tomato-based sauce, flavoured with ginger, garlic, chilli peppers and a variety of spices.'),
+('Idly', 'https://images.pexels.com/photos/4331489/pexels-photo-4331489.jpeg?auto=compress&cs=tinysrgb&w=600', 70, 1.99, true, false,'It made by steaming a batter consisting of fermented black lentils and rice.'),
+('Spring Roll', 'https://images.pexels.com/photos/6089654/pexels-photo-6089654.jpeg?auto=compress&cs=tinysrgb&w=600', 85, 1.99, true, false, 'It made by a pastry sheet is filled with vegetables, rolled & fried.'),
+('White Rice', 'https://images.pexels.com/photos/7593253/pexels-photo-7593253.jpeg?auto=compress&cs=tinysrgb&w=600', 206, 6.99, true, false, 'Basmathi rice.'),
+('Chicken Biriyani', 'https://media.istockphoto.com/id/1345624336/photo/chicken-biriyani.jpg?b=1&s=612x612&w=0&k=20&c=2CBPkgQI2PiNySSNi1tvwVSYFqOBgfgrgDPCyelIVrk=', 360, 16.99, false, true, 'It made up of rice, chicken and spices.'),
+('Fish Biriyani', 'https://media.istockphoto.com/id/1334383300/photo/fish-biryani-spicy-and-delicious-malabar-biryani-or-hydrabadi-biryani-dum-biriyani.jpg?b=1&s=612x612&w=0&k=20&c=T9VruqgYDkHJYyvooUXitzAYSPCBvPKakTM9s3n2VWs=', 296, 16.99, false, true, 'It made up of rice, fish and spices.'),
+('Shrimp Biriyani', 'https://media.istockphoto.com/id/479660444/photo/shrimps-curry-with-rice-on-plate-horizontal-view-from-above.jpg?b=1&s=612x612&w=0&k=20&c=SH1PHDac6ZQ1e7AjMeAeLonXP1vMDmO_PZlEHE-a5DY=', 210, 16.99, false, true, 'It made up of rice, fish and spices.'),
+('Vegetable Fried Rice', 'https://media.istockphoto.com/id/495188382/photo/indian-pulav-vegetable-rice-veg-biryani-basmati-rice.jpg?b=1&s=612x612&w=0&k=20&c=xnkpHKqSGa-MWPwth0_xBoQwrxJZN6IPsjf5Xfwbf2w=', 180, 13.99, true, false, 'It made up of rice and vegetables.'),
+('Chicken Kebab', 'https://images.pexels.com/photos/16423838/pexels-photo-16423838/free-photo-of-roasted-chicken-with-fries-and-salad.jpeg?auto=compress&cs=tinysrgb&w=600', 285, 3.50, false, true, 'The chicken is marinated in yogurt, olive oil, lemon, garlic and spices, and then grilled over an open flame until juicy and golden brown.'),
+('Mango Lassi', 'https://images.pexels.com/photos/14509267/pexels-photo-14509267.jpeg?auto=compress&cs=tinysrgb&w=600', 360, 4.99, true, false, 'It is a delicious and creamy blend of mangoes and yogurt with a touch of cardamom.'),
+('Gulab Jamun', 'https://media.istockphoto.com/id/1424509973/photo/gulab-jamun-an-indian-dessert.jpg?b=1&s=612x612&w=0&k=20&c=RCuPixqiDGiBbI92sp2RZy7sJ98M6JXoC0KqrVl9PpQ=', 125, 4.99, true, false, 'It is a dessert of fried dough balls that are soaked in a sweet, sticky sugar syrup.');
+
+
+
+
+
+
+
